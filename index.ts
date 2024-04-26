f3557cdcf6ba3912173acf70d091cee3619237ce
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-25 21:54:24 -0300

    feat: simple server express

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Ola Mundo");
+});
+
+app.listen(port, () => {
+  console.log(`Servidr rodando na porta: ${port}`);
+});
