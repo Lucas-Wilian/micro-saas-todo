a9a7a3a788601157deeea84807ebb5d2e7d2bffa
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-30 09:25:24 -0300

    feat: new route post: users | app.use

--- source/index.ts
+++ source/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import {
+  createUserController,
   findOneUserController,
   listUserController,
 } from "./controllers/user.controller";
 
 const app = express();
-
+app.use(express.json());
 const port = 3000;
 
 app.get("/users", listUserController);
+app.post("/users", createUserController);
 app.get("/users/:userId", findOneUserController);
 
 app.listen(port, () => {
