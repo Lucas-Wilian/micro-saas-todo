356f967168fe0dab2db3148af68f7c6b4b13a767
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-30 09:24:21 -0300

    create todo controller

---
+++ source/controllers/todo.controller.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+
+export const createTodoController = async (req: Request, res: Response) => {
+  const userId = req.headers["x-user-id"];
+
+  if (!userId) {
+    return res.status(403).send({
+      error: "Not Authorized",
+    });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId as string,
+    },
+  });
+
+  if (!user) {
+    return res.status(403).send({
+      error: "Not Authorized",
+    });
+  }
+
+  const { title } = req.body;
+  const todo = await prisma.todo.create({
+    data: {
+      title,
+      ownerId: user.id,
+    },
+  });
+  return res.send(todo);
+};
