1f5723e2a7b0ead47c39a63a2e56397c791904a2
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-30 10:08:15 -0300

    add file config stripe

---
+++ source/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  stripe: {
+    publishableKey:
+      "pk_test_51PBGAXF6wkJTwXjNsHrvdEWqTmNIEP3JV5qPFhI51dt9ozkQyse9YEnAdzagLfSUqIdvM6gxTQY9opLTqIhpBMLg006PIsOJu2",
+    secretKey:
+      "sk_test_51PBGAXF6wkJTwXjNhXIF1j6yNxaG8bKVuDuiquO8nD8riztO8liMYWlrSZEC3ONipvjkl4Cy90AXhd0S9E16OqGJ00Zo3Hf3ND",
+  },
+};
