0ac5f839b0c1ef4c2dbae23036707fbd558f5d5b
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-05-05 17:36:01 -0300

    new routes

--- source/index.ts
+++ source/index.ts
@@ -2,19 +2,29 @@ import express from "express";
 import {
   createUserController,
   findOneUserController,
-  listUserController,
+  listUsersController,
 } from "./controllers/user.controller";
 import { createTodoController } from "./controllers/todo.controller";
+import { createCheckoutController } from "./controllers/checkout.controller";
+import { stripeWebhookController } from "./controllers/stripe.controller";
 
 const app = express();
-app.use(express.json());
 const port = 3000;
 
-app.get("/users", listUserController);
+app.post(
+  "/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhookController
+);
+
+app.use(express.json());
+
+app.get("/users", listUsersController);
 app.post("/users", createUserController);
 app.get("/users/:userId", findOneUserController);
 app.post("/todos", createTodoController);
+app.post("/checkout", createCheckoutController);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
